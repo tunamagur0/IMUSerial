f20f6ab2c92c4a8be77fa813908db16f92de8b2f
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2019-10-03 20:04:41 +0900

    シリアル通信で得たデータをパース

--- SerialRead/Main.cpp
+++ SerialRead/Main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include "Serial.h"
 
-int main(void) {
-  Serial serial = Serial(3);
+struct vec3 {
+  float x;
+  float y;
+  float z;
+} acc, gyro;
 
-  serial.Start(115200);
+int main(void) {
+  const int serial_port = 3;
+  const int baud_rate = 115200;
+  Serial serial = Serial(serial_port);
+
+  serial.Start(baud_rate);
   char buffer[1024];
+  float pitch, roll, yaw, temp;
   while (true) {
     serial.Read(buffer);
-    std::cout << buffer << std::endl;
+    std::cout << sscanf(buffer, "g:%f,%f,%f a:%f,%f,%f p:%f r:%f y:%f t:%f",
+                        &gyro.x, &gyro.y, &gyro.z, &acc.x, &acc.y, &acc.z,
+                        &pitch, &roll, &yaw, &temp)
+              << std::endl;
+    std::cout << gyro.x << " " << gyro.y << " " << gyro.z << " " << acc.x << " "
+              << acc.y << " " << acc.z << " " << pitch << " " << roll << " "
+              << yaw << " " << temp << " " << std::endl;
   }
   serial.End();
   return 0;
